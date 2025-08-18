82e2076c519c09e64ebbc1d72112b447dc075779
VanTho <ovantho094@gmail.com>
2025-08-18 09:12:58 +0700

    connect railway mySql

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'MySql@VanTho0948',
-  database: 'stock_management',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "MySql@VanTho0948",
+  database: process.env.DB_NAME || "stock_management",
+  port: parseInt(process.env.DB_PORT || "3306", 10),
   waitForConnections: true,
   connectionLimit: 10,
 });
